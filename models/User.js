662ed404fefa0de810b6aca33be0f129c4a82eb7
Nguyen Quang An <ngquangan@gmail.com>
2019-04-14 03:11:05 +0700

    Initial project and create User schema

---
+++ models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new Schema({
+  email: {
+    type: String, required: true,
+    trim: true, unique: true,
+    match: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
+  },
+  firstName: {type: String},
+  lastName: {type: String}
+});
+
+module.exports = mongoose.model('User', UserSchema); 
