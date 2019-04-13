9f37ff916dfa69a7f6da624512dcd8112882175a
Nguyen Quang An <ngquangan@gmail.com>
2019-04-14 03:45:55 +0700

    Add swagger for api document

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const userRoute = require('./routes/user');
 
+
+const swaggerUi = require('swagger-ui-express'),
+    swaggerDocument = require('./swaggers/swagger.json');
+
 const app = express();
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/api/v1', userRoute);
 
